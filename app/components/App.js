250347cf6f4074a2838c703ff0f2df707d1cfae8
Santosh Bidve <santoshbidave2@gmail.com>
2017-06-28 16:58:52 -0700

    dynamic rendering done

--- app/components/App.js
+++ app/components/App.js
@@ -6,6 +6,7 @@ var Route = ReactRouter.Route;
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Results = require('./Results');
 var Switch = ReactRouter.Switch;
 
 class App extends React.Component{
@@ -17,6 +18,7 @@ class App extends React.Component{
           <Switch >
             < Route path="/popular" component={Popular} />
             < Route exact path="/" component={Home} />
+            < Route path="/battle/results" component = {Results} />
             < Route exact path="/battle" component={Battle} />
             < Route render = {function(){
               return <p>Not found!</p>
